e86e6e4a788f2f8b9d5dcf1e1b5d3ff6ec9a800e
Mohammed-Fathaah <mohammedfathaah@gmail.com>
2026-02-28 22:40:45 +0530

    form done..

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,31 @@
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 import { motion } from "framer-motion";
 import { FaGithub,FaLinkedin,FaEnvelope } from "react-icons/fa";
 
 function Contact(){
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_bb1uxtm",
+                "template_imxiv4a",
+                form.current,
+                "ptnGlr3CA1kg4yKh6"
+            )
+            .then(()=>{
+                alert("Message Sent Successfully!!");
+                form.current.reset();
+            },
+            (error)=> {
+                alert("Failed to send message.");
+                console.log(error);
+            }
+        );
+    }
+
     return (
         <section id="contact"
         className="min-h-screen px-6 py-20 bg-gray-100 dark:bg-black transition-colors duration-300">
@@ -15,6 +39,14 @@ function Contact(){
              className="max-w-3xl mx-auto bg-white dark:bg-gray-900 p-10 rounded-2xl shadow-lg">
                 <p className="text-center text-gray-600 dark:text-gray-300 mb-8">I'm open to Full Stack opportunities.Let's connect!</p>
 
+                {/* Contact form */}
+                <form ref={form} onSubmit={sendEmail} className="space-y-6 mb-10">
+                    <input type="text" name="name" placeholder="Your Name" required className="w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-800" />
+                    <input type="email" name="email" placeholder="Your Email" required className="w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-800" />
+                    <textarea name="title" placeholder="Your Message" rows="4" required className="w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-800"></textarea>
+                    <button type="submit" className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition">Send Message</button>
+                </form>
+                {/* Icons div */}
                 <div className="flex justify-center gap-8 text-3xl">
                     <a 
                      href="https://github.com/Mohammed-Fathaah"
@@ -34,7 +66,6 @@ function Contact(){
 
                       <a 
                        href="mailto:mohammedfathaah777@gmail.com"
-                       target="_blank"
                        rel="noopener noreferrer"
                        className="hover:text-blue-500 transition">
                         <FaEnvelope />
